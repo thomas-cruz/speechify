819c7c0b6a8ad7b084c5de5b44b97242d77c9195
Jacquelyn Amaya <jacquelyn@karta.com.au>
2024-04-05 14:02:06 +0800

    feat: updated fetchContent and parseContentIntoSentences functions

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -4,12 +4,29 @@ const API_URL = "http://localhost:5174/content";
  * Fetch the content from the api
  * In case of an error, return content as "<speak><s>There was an error</s></speak>"
  */
-const fetchContent = async (url = API_URL): Promise<string> => {};
+const fetchContent = async (url = API_URL): Promise<string> => {
+    const error = "<speak><s>There was an error</s></speak>";
+    try {
+        const res = await fetch(API_URL);
+        if (res.ok) {
+            const text = await res.text();
+            console.log({text})
+            return text;
+        }
+
+        return error;
+    } catch (err) {
+        return error;
+    }
+};
 
 /**
  * Parse the content into sentences, and return an array of sentences. Look at the Readme for sample input and expected output.
  * Avoid using DOMParser for implementing this function.
  */
-const parseContentIntoSentences = (content: string) => {};
+const parseContentIntoSentences = (content: string) => {
+    const sentences = content.match(/<s>(.*?)<\/s>/g);
+    return sentences ? sentences.map(s => s.replace(/<\/?s>/g, "")) : [];
+};
 
 export { fetchContent, parseContentIntoSentences };
